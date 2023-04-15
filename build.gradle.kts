a1130091969cd76f3a0b13a8f9fac0b9c38063f2
daz <daz@gradle.com>
2023-04-15 13:05:03 -0600

    Test with config cache
    - <project>/.gradle/8.0.2/executionHistory will contain both CONFIG_SECRET and TASK_SECRET
    - <project>/.gradle/configuration-cache will contain the CONFIG_SECRET, but not the TASK_SECRET

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,26 @@
-val greetingProvider = providers.environmentVariable("TASK_GREETING")
+val configEnvProvider = providers.environmentVariable("CONFIG_SECRET")
+val taskEnvProvider = providers.environmentVariable("TASK_SECRET")
 
 tasks.register<FooTask>("foo") {
-    greeting.set(greetingProvider)
+    greetingConf.set(configEnvProvider.get()) // Calling 'get()' forces this to be a configuration input
+    greetingTask.set(taskEnvProvider) // Not a configuration input
     targetFile.set(layout.buildDirectory.file("target.txt"))
 }
 
 @CacheableTask
 abstract class FooTask : DefaultTask() {
     @get:Input
-    abstract val greeting: Property<String>
+    abstract val greetingConf: Property<String>
+
+    @get:Input
+    abstract val greetingTask: Property<String>
 
     @get:OutputFile
     abstract val targetFile: RegularFileProperty
 
     @TaskAction
     fun execute() {
-        val msg = greeting.get()
+        val msg = greetingConf.get() + ':' + greetingTask.get()
         targetFile.get().asFile.writeText(msg)
         println(msg)
     }
