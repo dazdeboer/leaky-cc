1934ecbf73827cc70886a14204e42c2939bb1a2f
daz <daz@gradle.com>
2023-04-15 09:26:31 -0600

    Convert to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+val greetingProvider = providers.environmentVariable("TASK_GREETING")
+
+tasks.register<FooTask>("foo") {
+    greeting.set(greetingProvider)
+    targetFile.set(layout.buildDirectory.file("target.txt"))
+}
+
+abstract class FooTask : DefaultTask() {
+    @get:Input
+    abstract val greeting: Property<String>
+
+    @get:OutputFile
+    abstract val targetFile: RegularFileProperty
+
+    @TaskAction
+    fun execute() {
+        val msg = greeting.get()
+        targetFile.get().asFile.writeText(msg)
+        println(msg)
+    }
+}
